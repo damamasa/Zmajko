4b0187b6561820b3449a8debf7d83778b88cff0e
NikaBizek <106660879+NikaBizek@users.noreply.github.com>
2023-12-06 22:05:25 +0100

    Timer + optimiziran kod
    Dodan timer igre u gornjem lijevom kutu. Optimiziran kod za stvaranje neprijatelja, promijenjene varijable, uljepšan kod za lakše čitanje i mijenjanje varijabli (biti će korisno kada ćemo dodavati levele različitih težina).

--- main.py
+++ main.py
@@ -7,6 +7,12 @@ pygame.init()
 screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
 pygame.display.set_caption("Zmajevi")
 
+#font i tekst
+text_font = pygame.font.SysFont("Arial",50)
+def draw_text(text,font,text_col,x,y):
+    img = font.render(text,True,text_col)
+    screen.blit(img,(x,y))
+
 #slike
 zmaj_slika_og = pygame.image.load("slike/zmaj.png")
 pozadina = pygame.transform.scale(pygame.image.load("slike/pozadina.jpg"), (screen.get_width(), screen.get_height()))
@@ -22,28 +28,31 @@ def igra():
     zmaj_x = screen.get_width() - (screen.get_width()/2) - (zmaj_w/2)
     zmaj_y = screen.get_height() - (screen.get_height()/6) - (zmaj_h /2)
 
+    #protivnik varijable
+    ProtivnikX = []
+    ProtivnikY = []
+    protivnik_w = zmaj_w/2.5
+    protivnik_h = zmaj_h/2.5
+    brojProtivnika = 200
+
     #nova slika zmaja
     zmaj_slika = pygame.transform.scale(zmaj_slika_og, (zmaj_w, zmaj_h))
-    protivnik_slika = pygame.transform.scale(protivnik_og, (zmaj_w/2, zmaj_h/2))
+    protivnik_slika = pygame.transform.scale(protivnik_og, (protivnik_w, protivnik_h))
 
-    #protivnik varijable
-    listaProtivnikaX = []
-    listaProtivnikaY = []
-    brojProtivnika = 200 #za sad
-
-    for i in range(brojProtivnika):
-        protivnikX = random.randint(0, screen.get_width()-zmaj_w/2)
-        if i == 0:
-            listaProtivnikaX.append(protivnikX)
-        else:
-            while abs(listaProtivnikaX[i-1]-protivnikX) < zmaj_w:
-                protivnikX = random.randint(0, screen.get_width()-(zmaj_w/2))
-            else:
-                listaProtivnikaX.append(protivnikX)
-        if i == 0:
-            listaProtivnikaY.append(-zmaj_h)
-        else:
-            listaProtivnikaY.append(listaProtivnikaY[i-1]-zmaj_h)
+    def stvoriProtivnike():
+        for i in range(brojProtivnika):
+            noviX = random.randint(0, screen.get_width()-protivnik_w) #stvaranje x pozicije protivnika koja može bit od početka do kraja screena
+            if i == 0: #za prvog protivnika uzima se bilo koji x
+                ProtivnikX.append(noviX)
+            else: #za ostale protivnike se uzima bilo koji x, ali ako je x u blizini prijašnjeg neprijatelja, bira se novi x. Tako neće doći do preklapanja protivnika
+                while abs(ProtivnikX[i-1]-noviX) < protivnik_w*2:
+                    noviX = random.randint(0, screen.get_width()-protivnik_w)
+                else:
+                    ProtivnikX.append(noviX)
+            if i == 0: #uzima se određeni y na kome će se stvoriti 1. neprijatelj
+                ProtivnikY.append(-protivnik_h*2)
+            else: #ostali neprijatelji uzimaju prošli y i stvaraju se za istu visinu višu od prošloga
+                ProtivnikY.append(ProtivnikY[i-1]-protivnik_h*2)
 
     def igrac(x,y):
         screen.blit(zmaj_slika, (x, y))
@@ -92,21 +101,23 @@ def igra():
         if zmaj_y + zmaj_h> screen.get_height():
             zmaj_y = screen.get_height() - zmaj_h
         
-        #vrijeme
-        t2 = time.perf_counter()
-        vrijeme = vrijeme + (t2-t1)*1000
+        stvoriProtivnike() #stvaranje neprijatelja
 
-        #stvaranje neprijatelja
-        for i in range(brojProtivnika):
-            listaProtivnikaY[i] += protivnikPomakY
-            protivnikX = random.randint(0, screen.get_width()-zmaj_w/2)
-            protivnik(listaProtivnikaX[i],listaProtivnikaY[i], i)
+        for i in range(brojProtivnika): #micanje i crtanje protivnika
+            ProtivnikY[i] += protivnikPomakY
+            protivnik(ProtivnikX[i],ProtivnikY[i], i)
 
-            if pygame.Rect(listaProtivnikaX[i], listaProtivnikaY[i], zmaj_w/2, zmaj_h/2).colliderect(pygame.Rect(zmaj_x,zmaj_y,zmaj_w,zmaj_h)):
+            if pygame.Rect(ProtivnikX[i], ProtivnikY[i], protivnik_w, protivnik_h).colliderect(pygame.Rect(zmaj_x,zmaj_y,zmaj_w,zmaj_h)): #collision protivnika
                 zmaj_x = 0
                 zmaj_y = 0
+
         #crtanje zmaja
         igrac(zmaj_x,zmaj_y)
+
+        #vrijeme
+        t2 = time.perf_counter()
+        vrijeme =round(vrijeme + ((t2-t1)*10),2)
+        draw_text(f"{vrijeme}s",text_font,(0,0,0),0,0)
 
         pygame.display.update()
 igra()
